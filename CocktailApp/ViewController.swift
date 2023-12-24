51110e82356bb151f47c0a77c175a0e810b338a5
Ensar Öztürk <ozturkkensar@gmail.com>
2023-12-24 23:00:55 +0300

    first commit

--- CocktailApp/ViewController.swift
+++ CocktailApp/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        
+        print("hello world")
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
