7d750d62e62c07ed724e77cde6126f21fd3840bb
Ensar Öztürk <ozturkkensar@gmail.com>
2023-12-25 18:42:33 +0300

    Category cell references were given

--- CocktailApp/Api/Module/CategoryCell/CategoryCell.swift
+++ CocktailApp/Api/Module/CategoryCell/CategoryCell.swift
@@ -8,11 +8,18 @@
 import UIKit
 
 class CategoryCell: UICollectionViewCell {
-
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    
     static let identifier = "CategoryCell"
     override func awakeFromNib() {
         super.awakeFromNib()
         
     }
-
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        // label
+        label.contentMode = .center
+    }
 }
