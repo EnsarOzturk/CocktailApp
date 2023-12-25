c228e01ba6eb7fed365dc5c28981ad09e7aaab1f
Ensar Öztürk <ozturkkensar@gmail.com>
2023-12-25 15:00:09 +0300

    CategoryEndpointItem created

---
+++ CocktailApp/EndPointItems/CategoryEndpointItem.swift
@@ -0,0 +1,12 @@
+//
+//  CategoryEndpointItem.swift
+//  CocktailApp
+//
+//  Created by Ensar on 25.12.2023.
+//
+
+import Foundation
+
+struct CategoryEndpointItem: Endpoint {
+    var path = "/list.php?c=list"
+}
