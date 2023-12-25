783c725ac71683d7201ca420f112009c116ce42c
Ensar Öztürk <ozturkkensar@gmail.com>
2023-12-25 16:31:19 +0300

    Category cell created

---
+++ CocktailApp/Api/Module/CategoryCell/CategoryCell.swift
@@ -0,0 +1,17 @@
+//
+//  CategoryCell.swift
+//  CocktailApp
+//
+//  Created by Ensar on 25.12.2023.
+//
+
+import UIKit
+
+class CategoryCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+}
