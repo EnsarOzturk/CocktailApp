e98f7d053c0e915100ad66149c364f3eed1ae0d5
Ensar Öztürk <ozturkkensar@gmail.com>
2023-12-26 13:25:22 +0300

    Search view controller collectionView created

--- CocktailApp/Api/Controllers/SearchViewController.swift
+++ CocktailApp/Api/Controllers/SearchViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class SearchViewController: UIViewController {
-
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
