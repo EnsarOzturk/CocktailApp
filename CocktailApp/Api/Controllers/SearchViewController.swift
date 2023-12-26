c7e6fdc64af9c614680bb8726c4f09879121e67c
Ensar Öztürk <ozturkkensar@gmail.com>
2023-12-26 13:44:36 +0300

    Search view controller inheritances were received

--- CocktailApp/Api/Controllers/SearchViewController.swift
+++ CocktailApp/Api/Controllers/SearchViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 class SearchViewController: UIViewController {
     @IBOutlet weak var collectionView: UICollectionView!
     private var drinks: [Cocktail] = []
+    private let network = NetworkManager()
+    private var filteredDrinks: [Cocktail] = []
+    private var isSearching: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
