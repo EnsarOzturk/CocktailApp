25b39ab492788585f12fe60a278f877d2394c6b0
Ensar Öztürk <ozturkkensar@gmail.com>
2023-12-27 15:43:17 +0300

    SmallCardCell created

---
+++ CocktailApp/Api/Module/SearchListCell/SmallCardCell.swift
@@ -0,0 +1,17 @@
+//
+//  SmallCardCell.swift
+//  CocktailApp
+//
+//  Created by Ensar on 27.12.2023.
+//
+
+import UIKit
+
+class SmallCardCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
