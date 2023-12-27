ac65ab8e892ee3da6baebe5ec52a04a15649ce04
Ensar Öztürk <ozturkkensar@gmail.com>
2023-12-27 23:39:50 +0300

    BigCardCell created

---
+++ CocktailApp/Api/Module/ListCell/BigCardCell.swift
@@ -0,0 +1,17 @@
+//
+//  BigCardCell.swift
+//  CocktailApp
+//
+//  Created by Ensar on 27.12.2023.
+//
+
+import UIKit
+
+class BigCardCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
