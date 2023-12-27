b880187f2110b5ad5c93b57a8968944b23ef3d0e
Ensar Öztürk <ozturkkensar@gmail.com>
2023-12-27 12:20:30 +0300

    NetworkManager was updated

--- CocktailApp/Api/Controllers/SearchViewController.swift
+++ CocktailApp/Api/Controllers/SearchViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 
-class SearchViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UISearchResultsUpdating, UICollectionViewDelegateFlowLayout {
+class SearchViewController: UIViewController {
   
     @IBOutlet weak var collectionView: UICollectionView!
     var cocktails: [Cocktail] = []
@@ -19,18 +19,20 @@ class SearchViewController: UIViewController, UICollectionViewDataSource, UIColl
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // collection
         collectionView.dataSource = self
         collectionView.delegate = self
         collectionView.register(UINib(nibName: "SearchListCell", bundle: nil), forCellWithReuseIdentifier: SearchListCell.identifier)
-        
+        searchUpdate()
+    }
+    
+    private func searchUpdate() {
         searchController = UISearchController(searchResultsController: nil)
-             searchController.searchResultsUpdater = self
-             searchController.obscuresBackgroundDuringPresentation = false
-             searchController.searchBar.placeholder = "Search Cocktails"
-
-             navigationItem.searchController = searchController
-             definesPresentationContext = true
-        
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search Cocktails"
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
     }
     
     func fetchCocktails(for searchQuery: String) {
@@ -46,41 +48,6 @@ class SearchViewController: UIViewController, UICollectionViewDataSource, UIColl
             }
         }
     
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        if isFiltering() {
-                    return filteredCocktails.count
-                }
-                return cocktails.count
-    }
-    
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SearchListCell.identifier, for: indexPath) as! SearchListCell
-
-                let cocktail: Cocktail
-                if isFiltering() {
-                    cocktail = filteredCocktails[indexPath.item]
-                } else {
-                    cocktail = cocktails[indexPath.item]
-                }
-
-                cell.configure(with: cocktail)
-
-                return cell
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let width = (UIScreen.main.bounds.width - 48) / 3
-        let height = width / 2 * 3
-        return CGSize(width: width, height: height)
-    }
-    
-    func updateSearchResults(for searchController: UISearchController) {
-        guard let searchText = searchController.searchBar.text else { return }
-                filterContentForSearchText(searchText)
-        fetchCocktails(for: searchText)
-    }
-    
     func filterContentForSearchText(_ searchText: String) {
           filteredCocktails = cocktails.filter { $0.strDrink.lowercased().contains(searchText.lowercased()) }
           collectionView.reloadData()
@@ -92,6 +59,44 @@ class SearchViewController: UIViewController, UICollectionViewDataSource, UIColl
     func searchBarIsEmpty() -> Bool {
           return searchController.searchBar.text?.isEmpty ?? true
       }
+}
+extension SearchViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if isFiltering() {
+                    return filteredCocktails.count
+                }
+                return cocktails.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SearchListCell.identifier, for: indexPath) as! SearchListCell
+
+                let cocktail: Cocktail
+                if isFiltering() {
+                    cocktail = filteredCocktails[indexPath.item]
+                } else {
+                    cocktail = cocktails[indexPath.item]
+                }
+                    cell.configure(with: cocktail)
+
+                return cell
+    }
+}
+
+extension SearchViewController: UISearchResultsUpdating {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = (UIScreen.main.bounds.width - 48) / 3
+        let height = width / 2 * 3
+        return CGSize(width: width, height: height)
+    }
+}
+
+extension SearchViewController: UICollectionViewDelegateFlowLayout {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let searchText = searchController.searchBar.text else { return }
+                filterContentForSearchText(searchText)
+                fetchCocktails(for: searchText)
+    }
 }
 
 
--- CocktailApp/Api/Service/NetworkManager.swift
+++ CocktailApp/Api/Service/NetworkManager.swift
@@ -8,23 +8,30 @@
 import Foundation
 import Alamofire
 
-class NetworkManager {
-    func request<T: Codable>(type: T.Type, item: Endpoint, completion: @escaping(Result<T, Error>) -> Void) {
-        AF.request(item.url, method: item.method).responseData { response in
 
+class NetworkManager {
+    func request<T: Codable>(type: T.Type, item: Endpoint, completion: @escaping (Result<T, Error>) -> Void) {
+        AF.request(item.url, method: item.method).responseData { response in
             switch response.result {
             case .success(let data):
                 print(response.request?.url)
                 print(item.path)
                 let jsonString = String(data: data, encoding: .utf8)
                 print("API Response: \(jsonString ?? "No Data")")
-                let response = try! JSONDecoder().decode(T.self, from: data)
-                completion(.success(response))
-            case .failure(let error):
-                print("error\(error)")
-                completion(.failure(error))
 
+                do {
+                    let decodedResponse = try JSONDecoder().decode(T.self, from: data)
+                    completion(.success(decodedResponse))
+                } catch {
+                    print("Decoding error: \(error)")
+                    completion(.failure(error))
+                }
+                
+            case .failure(let error):
+                print("Error: \(error)")
+                completion(.failure(error))
             }
         }
     }
 }
+
