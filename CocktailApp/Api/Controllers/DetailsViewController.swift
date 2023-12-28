99c6376dddb7811ebd4a0a1239a638104c711b33
Ensar Öztürk <ozturkkensar@gmail.com>
2023-12-28 10:39:24 +0300

    DetailsViewController created

---
+++ CocktailApp/Api/Controllers/DetailsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailsViewController.swift
+//  CocktailApp
+//
+//  Created by Ensar on 28.12.2023.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
