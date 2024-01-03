dc9056d2b72e8e584de9d75a2444128f7a7c139e
Ensar Öztürk <ozturkkensar@gmail.com>
2024-01-03 22:06:54 +0300

    A mechanism that brings random cocktails has been created

--- CocktailApp/Api/Controllers/RandomCocktailViewController.swift
+++ CocktailApp/Api/Controllers/RandomCocktailViewController.swift
@@ -9,38 +9,49 @@ import UIKit
 import SDWebImage
 
 class RandomCocktailViewController: UIViewController {
-  
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var nameLabel: UILabel!
-    
     private let randomCocktailService = RandomCocktailService()
     private let network = NetworkManager()
     private var cocktails: [RandomCocktail] = []
+    private var timer: Timer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-    }
-    
-    @IBAction func bringRandomButtonTapped(_ sender: UIButton) {
         fetchRandomCocktail()
     }
     
+    @IBAction func bringRandomButtonTapped(_ sender: UIButton) {
+        startUpdatingCocktails()
+    }
+    
+    func startUpdatingCocktails() {
+        timer?.invalidate()
+        timer = nil
+        timer = Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateRandomCocktail), userInfo: nil, repeats: true)
+        RunLoop.main.add(timer!, forMode: .common)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) { [weak self] in
+            self?.timer?.invalidate()
+            self?.timer = nil
+        }
+        updateRandomCocktail()
+    }
+    
+    @objc func updateRandomCocktail() {
+        fetchRandomCocktail()
+    }
+
     func fetchRandomCocktail() {
          randomCocktailService.getRandomCocktail { [weak self] result in
              switch result {
              case .success(let randomCocktailResponse):
                  if let randomCocktail = randomCocktailResponse.drinks.first {
-                     self?.updateUI(with: randomCocktail)
-                 } else {
-                     self?.showError(message: "No random cocktails found.")
-                 }
+                    self?.updateUI(with: randomCocktail) }
              case .failure(let error):
                  self?.showError(message: "Error fetching random cocktail: \(error.localizedDescription)")
              }
          }
      }
-    
     func updateUI(with cocktail: RandomCocktail) {
            nameLabel.text = cocktail.strDrink
            if let imageUrl = URL(string: cocktail.strDrinkThumb ?? "") {
