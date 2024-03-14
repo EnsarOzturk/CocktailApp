32d4594bb587afb9d0e9db1edc60a272faabf97b
Ensar Öztürk <ozturkkensar@gmail.com>
2024-03-14 15:03:18 +0300

    Request fulfilled

--- CocktailApp/Api/Model/ViewModels/CategoryViewModel.swift
+++ CocktailApp/Api/Model/ViewModels/CategoryViewModel.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+class CategoryViewModel {
+    private let networkManager: NetworkManager
+    private var categories: [Category] = []
+    
+    init(networkManager: NetworkManager) {
+        self.networkManager = networkManager
+    }
+    
+    func fetchCategories(completion: @escaping (Result<[Category], Error>) -> Void) {
+        networkManager.request(type: CategoryResponse.self, item: CategoryEndpointItem()) { result in
+            switch result {
+            case .success(let response):
+                if let categories = response.drinks {
+                    self.categories = categories
+                    completion(.success(categories))
+                }
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
