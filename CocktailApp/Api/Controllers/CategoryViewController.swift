cb5ca28efda26740164d612f269fd9183417aacd
Ensar Öztürk <ozturkkensar@gmail.com>
2024-03-15 14:35:50 +0300

    CategoryViewModel edited

--- CocktailApp/Api/Controllers/CategoryViewController.swift
+++ CocktailApp/Api/Controllers/CategoryViewController.swift
@@ -9,12 +9,12 @@ import UIKit
 
 class CategoryViewController: UIViewController{
     @IBOutlet weak var collectionView: UICollectionView!
-    private let network = NetworkManager()
-    private var categories: [Category] = []
-    
+    private var viewModel: CategoryViewModel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // collection
+        viewModel = CategoryViewModel(networkManager: NetworkManager())
         collectionView.dataSource = self
         collectionView.delegate = self
         collectionView.register(UINib(nibName: "CategoryCell", bundle: nil), forCellWithReuseIdentifier: CategoryCell.identifier)
@@ -28,17 +28,16 @@ class CategoryViewController: UIViewController{
         tabBarController?.tabBar.barTintColor = UIColor.white
         tabBarController?.tabBar.tintColor = UIColor.black
         // fetch network
-        network.request(type: CategoryResponse.self, item: CategoryEndpointItem()) { result in
-            switch result {
-            case .success(let response):
-                if let categories = response.drinks {
-                    self.categories = categories
+        viewModel.fetchCategories { [weak self] result in
+                guard let self = self else { return }
+                switch result {
+                case .success:
                     DispatchQueue.main.async {
                         self.collectionView.reloadData()
-              }
+                    }
+                case .failure(let error):
+                    print("Error fetching categories: \(error)")
             }
-            case .failure(let error): print(error)
-          }
         }
     }
     
@@ -50,33 +49,24 @@ class CategoryViewController: UIViewController{
 
 extension CategoryViewController: UICollectionViewDataSource, UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        categories.count
+        viewModel.numberOfCategories()
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CategoryCell.identifier, for: indexPath) as! CategoryCell
-        let category: Category
-        category = categories[indexPath.row]
-        cell.configure(category: category)
-        return cell
-    }
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CategoryCell.identifier, for: indexPath) as? CategoryCell else {
+            fatalError("Could not dequeue CategoryCell")
+        }
+            if let category = viewModel.getCategory(at: indexPath.row) {
+                cell.configure(category: category)
+            }
+            return cell
+        }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let selectedCategory = categories[indexPath.row].strCategory
-        if let selectedCategory = categories.first(where: { $0.strCategory == selectedCategory }) {
-            listViewController(category: selectedCategory)
-        } else {
-      }
+        if let category = viewModel.getCategory(at: indexPath.row) {
+                   listViewController(category: category)
+        }
     }
-    
-    func listViewController(category: Category) {
-        let listVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "ListViewController") as! ListViewController
-        if let navigationController = navigationController {
-            listVC.selectedCategory = category
-            navigationController.pushViewController(listVC, animated: true)
-        } else {
-    }
-}
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return 8
@@ -90,3 +80,12 @@ extension CategoryViewController: UICollectionViewDelegateFlowLayout {
         return CGSize(width: width, height: height)
     }
 }
+
+extension CategoryViewController {
+    func listViewController(category: Category) {
+        let listVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "ListViewController") as! ListViewController
+            listVC.selectedCategory = category
+            navigationController?.pushViewController(listVC, animated: true)
+  }
+}
+    
