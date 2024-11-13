dda2dd357f8410ba79957e1dc263b5b7f42707fd
Ensar Öztürk <ozturkkensar@gmail.com>
2024-11-13 14:37:33 +0300

    Updated RandomCocktailService

--- CocktailApp/Api/Service/RandomCocktailService.swift
+++ CocktailApp/Api/Service/RandomCocktailService.swift
@@ -9,14 +9,29 @@ import Foundation
 import Alamofire
 
 class RandomCocktailService {
-    func getRandomCocktail(completion: @escaping (Result<RandomCocktailResponse, Error>) -> Void) {
+    func getRandomCocktail(completion: @escaping (Result<RandomCocktailResponse, NetworkError>) -> Void) {
         let endpoint = RandomCocktailEndpointItem()
+        
         AF.request(endpoint.url, method: endpoint.method).validate().responseDecodable(of: RandomCocktailResponse.self) { response in
             switch response.result {
             case .success(let data):
                 completion(.success(data))
             case .failure(let error):
-                completion(.failure(error))
+                // Alamofire hata türünü NetworkError’a çeviriyoruz
+                let networkError: NetworkError
+                if let afError = error.asAFError {
+                    switch afError {
+                    case .responseValidationFailed(reason: .dataFileNil):
+                        networkError = .noData
+                    case .responseSerializationFailed(reason: .decodingFailed):
+                        networkError = .decodingFailed
+                    default:
+                        networkError = .requestFailed
+                    }
+                } else {
+                    networkError = .custom(message: error.localizedDescription)
+                }
+                completion(.failure(networkError))
             }
         }
     }
--- CocktailApp/SceneDelegate.swift
+++ CocktailApp/SceneDelegate.swift
@@ -10,20 +10,18 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
-           func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-               guard let windowScene = (scene as? UIWindowScene) else { return }
-
-               let window = UIWindow(windowScene: windowScene)
-               
-               // Set LaunchScreenViewController as the initial view controller
-               let launchVC = LaunchScreenViewController()
-               window.rootViewController = launchVC
-               self.window = window
-               window.makeKeyAndVisible()
-           }
     
-
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let window = UIWindow(windowScene: windowScene)
+        let launchVC = LaunchScreenViewController()
+        window.rootViewController = launchVC
+        self.window = window
+        window.makeKeyAndVisible()
+    }
+    
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
